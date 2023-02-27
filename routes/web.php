d0238187c08d37584bca7f67b88dbabdcbcc4432
Marc Collet <marc.collet@anemshield.net>
2023-02-27 15:08:16 +0000

    adding test on controller

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@
 use Illuminate\Support\Facades\Route;
 use Ykmar\VueI18nTranslationLoader\Controllers\TranslationController;
 
-Route::get('translations/{translationName}', [TranslationController::class, 'transform'])->middleware('auth');
+Route::get('translations/{translationName}', [TranslationController::class, 'transform']);
--- tests/TranslationControllerTest.php
+++ tests/TranslationControllerTest.php
@@ -2,13 +2,33 @@
 
 namespace Ykmar\VueI18nTranslationLoader\Tests;
 
-use PHPUnit\Framework\TestCase;
+use Orchestra\Testbench\TestCase;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Lang;
+use Ykmar\VueI18nTranslationLoader\Controllers\TranslationController;
 
 class TranslationControllerTest extends TestCase
 {
     /** @test */
-    public function can_transform_translation()
+    public function can_transform_translations_for_vuei18n()
     {
+        Lang::partialMock()
+            ->shouldReceive('get')
+            ->with('test')
+            ->andReturn($this->getMockedTranslations());
 
+        $translationController = new TranslationController();
+
+        $response = $translationController->transform('test');
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertJson('{"test.test":"This is a test"}');
+    }
+
+    private function getMockedTranslations(): array
+    {
+        return [
+            'test' => 'This is a test'
+        ];
     }
 }
